afa992fe0b7e17b1f543ec1d2c876c8789c9d481
Jasper Spierling <jasper.spierling@googlemail.com>
2019-11-21 02:33:12 +0100

    changed names of classes

--- simulation.py
+++ simulation.py
@@ -1,15 +1,14 @@
 import simpy
-from model_piecewise_one_func import optimal_quantities
-from model_piecewise_one_func import update_params
+from MIP import optimal_quantities
+from MIP import update_params
 
-update_params(lead=[1,1])
+update_params(lead=[1, 1])
 
 print("FIRST SIM: ", optimal_quantities())
 
-update_params(c_holding=[5,5])
+update_params(c_holding=[5, 5])
 
 print("SECOND SIM: ", optimal_quantities())
 
 env = simpy.Environment()
 env.run(until=10)
-
