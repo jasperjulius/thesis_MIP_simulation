b6e3493ce6b0cd9bdf61859feb68699d8fcb5d67
Jasper Spierling <jasper.spierling@googlemail.com>
2019-12-16 13:34:28 +0100

    excel print out including R of retailers

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ def print_results_to_sheet(results, sheet, offset_row, start_column):
 # todo: 1. gleiche parameter (auch R), verschiedene random values -> große schwankungen bei mip - mit höheren werten für R?
 # todo: alles soweit niederschreiben über implementierung - paper lesen - literaturteil thomas lesen
 
-reps = 90
+reps = 80
 R_start = 10
 
 exec_times = []
@@ -25,7 +25,7 @@ wb = openpyxl.load_workbook('/Users/jasperinho/PycharmProjects/thesis_MIP/genera
 length = 10000
 
 sheet = wb[wb.sheetnames[0]]
-sheet["AB4"] = length
+sheet["AD4"] = length
 
 for current in range(reps):
     sim = simulation.Simulation(length=length, stock=100, stochastic=True)
@@ -36,27 +36,29 @@ for current in range(reps):
     r1.c_holding = 0.3
     r1.c_shortage = 4
 
-    sim.warehouse.R = R_start +current # + trunc(current / 10)*10
+    sim.warehouse.R = R_start + current  # + trunc(current / 10)*10
 
     sheet["A%d" % (first_row + current)] = sim.warehouse.R
-    sheet["B%d" % (first_row + current)] = sim.seed
+    sheet["B%d" % (first_row + current)] = sim.warehouse.retailers[0].R
+    sheet["C%d" % (first_row + current)] = sim.warehouse.retailers[1].R
+    sheet["D%d" % (first_row + current)] = sim.seed
     sim.run(FIFO=False)
     after1 = time.time()
     results_mip = sim.collect_statistics()
     sim.reset()
 
-    print_results_to_sheet(results_mip, sheet, current, 5)
+    print_results_to_sheet(results_mip, sheet, current, 7)
 
     sim.run(FIFO=True)
     results_fifo = sim.collect_statistics()
     sim.reset()
 
-    print_results_to_sheet(results_fifo, sheet, current, 16)
+    print_results_to_sheet(results_fifo, sheet, current, 18)
     print(results_mip, results_fifo)
     after2 = time.time()
 
-    sheet["C%d" % (first_row + current)] = after1 - last_time
-    sheet["D%d" % (first_row + current)] = after2 - after1
+    sheet["E%d" % (first_row + current)] = after1 - last_time
+    sheet["F%d" % (first_row + current)] = after2 - after1
     last_time = after2
 
 wb.save("generated_sheets/current.xlsx")
