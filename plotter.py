5cf04706e095c5ecbc5d53fc638f96f583d3af76
Jasper Spierling <jasper.spierling@googlemail.com>
2020-03-02 18:35:24 +0100

    got four plots now

--- plotter.py
+++ plotter.py
@@ -1,6 +1,9 @@
 import matplotlib.pyplot as plt
 from combine_graphs import combine
+from matplotlib import rc
+from matplotlib.pyplot import MaxNLocator
 
+rc('text', usetex=True)
 
 def expected_invs():
     x = []
@@ -42,36 +45,67 @@ current_inv = 24
 mu = 10
 x_max = 40
 y_max = 15
-
+x_label = "amount sent to retailer $i$"
 figure1 = plt.figure(1, figsize=(6.4, 6.8))
 invs = expected_invs()
 
 chart_multiple = figure1.add_subplot(211)
 chart_multiple.axis([0, x_max, 0, 12])
-chart_multiple.set_xlabel("amount sent to retailer i")
-chart_multiple.set_ylabel("expected costs in period t = 2, 3 and 4")
+chart_multiple.set_xlabel(x_label)
+chart_multiple.set_ylabel("$S_{i, t}(x_i) + H_{i, t}(x_i)$ for $t \in \{ 2, 3, 4\}$")
 lists_x = [list_x_single(i, x_max) for i in invs]
 lists_y = [list_y_single(i) for i in lists_x]
 colors = [(23 / 255, 118 / 255, 182 / 255), (1, 0.5, 0), (39 / 255, 163 / 255, 37 / 255)]
 for c, x, y in zip(colors, lists_x, lists_y):
     chart_multiple.plot(x, y, "--", color=c)
+
 colors.reverse()
 colors.append("b")
 colors.reverse()
+
 y_max_combined = 40
 chart_combined = figure1.add_subplot(212)
 chart_combined.axis([0, x_max, 0, y_max_combined])
-chart_combined.set_xlabel("amount sent to retailer i")
-chart_combined.set_ylabel("sum of expected costs")
+chart_combined.set_xlabel(x_label)
+chart_combined.set_ylabel("$\sum_{t \in {2, 3, 4}} (S_{i, t}(x_i) + H_{i, t}(x_i))$")
 pairs = list(map(list, zip(lists_x, lists_y)))
 lc = [list(zip(*i)) for i in pairs]
 one_list_combined = combine(lc[0], 1, lc[1], 1, lc[2], 1)
-print(one_list_combined)
 list_x, list_y = zip(*one_list_combined)
 chart_combined.plot(list_x, list_y, "r")
 for c, x, y in zip(colors, list_x, list_y):
     y_min = y / y_max_combined
     chart_combined.axvline(x=x, ymin=y_min, ls=':', color=c)
+# plt.show()
+# figure1.savefig("./multicombined")
+plt.clf()
+
+figure2 = plt.figure(2, figsize=(6.4, 3.8))
+chart_single = figure2.add_subplot(111)
+chart_single.axis([0, x_max, 0, 12])
+chart_single.set_xlabel(x_label)
+chart_single.set_ylabel("$S_{i, t}(x_i) + H_{i, t}(x_i)$ for $t = 2$")
+chart_single.plot(lists_x[0], lists_y[0], "--")
+# plt.show()
+# figure2.savefig("./single")
+plt.clf()
+
+figure3 = plt.figure(3, figsize=(4.8, 4.4))
+chart_fcfs = figure3.add_subplot(111)
+chart_fcfs.axis([-1, 0.1, 0, 27])
+chart_fcfs.set_xlabel("period $t$", fontsize=14)
+chart_fcfs.set_ylabel("inventory", fontsize=14)
+chart_fcfs.xaxis.set_major_locator(MaxNLocator(integer=True))
+chart_fcfs.plot([-1, 0], [25, 3])
+chart_fcfs.plot([-1, 0.1], [17, 17], "--", color="orange")
+chart_fcfs.yaxis.set_ticks([3, 17, 25])
+chart_fcfs.yaxis.set_ticklabels(["$IP_i$", "$R_i$", "$IP_i + d$"], fontsize=14)
 plt.show()
-figure1.savefig("./testfigure")
-# latex mathe in axenbezeichnung
+figure3.savefig("./fcfs")
+# plt.clf()
+
+figure3 = plt.figure(3, figsize=(6.4, 3.8))
+chart_fcfs = figure3.add_subplot(111)
+# continue: final figure as in power point (plus estimated demands? or second one)?
+
+
